d1d143d10f7b5168b5feb065ed38a57ac492d319
marina <marilimatorres26@gmail.com>
2023-06-28 15:55:54 -0300

    construtores e funções backlog.hpp

--- lp1_scrum/src/backlog.hpp
+++ lp1_scrum/src/backlog.hpp
@@ -1,29 +1,51 @@
 #include "../include/backlog.h"
 
+Backlog::Backlog(){
+  this->cauda = NULL;
+  this->cabeca = NULL;
+  this->quantidade = 0;
+}
+
+Backlog::Backlog(Tarefa * cabeca, Tarefa * cauda, int quantidade){
+  this->cauda = cauda;
+  this->cabeca = cabeca;
+  this->quantidade = 0;
+}
+
+Backlog::~Backlog(){}
+
 void Backlog::setCabeca(Tarefa * h){
-  
+  this->cabeca = h;
 }
+
 Tarefa* Backlog::getCabeca(){
-  
+  return this->cabeca;
 }
+
 void Backlog::setCauda(Tarefa * t){
-  
+  this->cauda = t;  
 }
+
 Tarefa* Backlog::getCauda(){
-  
+  return this->cauda;  
 }
+
 void Backlog::setQuantidade(int quantidade){
-          
+  this->quantidade = quantidade;
 }
+
 int Backlog::getQuantidade(){
-          
+  return this->quantidade;          
 }
+
 void Backlog::addTarefa(Tarefa * t){
           
 }
+
 void Backlog::deletarTarefa(Tarefa * t){
           
 }
+
 Tarefa* Backlog::consultar(){
         
 } //mostrar tarefas do backlog
