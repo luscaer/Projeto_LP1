ccc9efa17bdf057dcc82bae6cbaf5d8c269ff1d8
marina <marilimatorres26@gmail.com>
2023-06-28 15:26:21 -0300

    funcoes pessoa.h

--- lp1_scrum/src/pessoa.hpp
+++ lp1_scrum/src/pessoa.hpp
@@ -1,25 +1,46 @@
 #include "../include/pessoa.h"
 
+Pessoa::Pessoa(){
+  this->nome = "";
+  this->funcao = "";
+  this->proximo = NULL;
+}
+
+Pessoa::Pessoa(string nome, string funcao){
+  this->nome = nome;
+  this->funcao = funcao;
+  this->proximo = NULL; 
+}
+
+Pessoa::Pessoa(Pessoa *proximo, string nome, string funcao){
+  this->nome = nome;
+  this->funcao = funcao;
+  this->proximo = proximo; 
+}
+
+Pessoa::~Pessoa(){}
+
+
 void Pessoa::setProximo(Pessoa* proximo){
-  
+  this->proximo = proximo;
 }
 
 Pessoa* Pessoa::getProximo(){
-
+  return this->proximo;
 }
 
 void Pessoa::setNome(string nome){
-  
+  this->nome = nome;
 }
 
 string Pessoa::getNome(){
-  return "";
+  return this->nome;
 }
 
 void Pessoa::setFuncao(string funcao){
-  
+  this->funcao = funcao;
 }
 
 string Pessoa::setFuncao(){
-  return "";
+  return this->funcao;
 }
